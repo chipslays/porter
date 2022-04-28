5e5874d3eb4f82a3d920f44a7dd41e7b5439e885
chipslays <gorshkov34@gmail.com>
2022-04-28 15:40:21 +0400

    update with validation

--- examples/basic/Events/HelloToEvent.php
+++ examples/basic/Events/HelloToEvent.php
@@ -9,8 +9,17 @@ class HelloToEvent extends AbstractEvent
 {
     public static string $eventId = 'hello to';
 
+    protected array $rules = [
+        'username' => ['stringType', ['length', [4, 18]]],
+    ];
+
     public function handle(TcpConnection $connection, Payload $payload, Server $server): void
     {
+        if ($this->hasErrors()) {
+            $this->reply('bad request', ['errors' => $this->errors]);
+            return;
+        }
+
         $username = $this->payload->data['username'];
         $this->reply(data: ['message' => "Hello, {$username}!"]);
     }
