1c05596a139de4570bbc0e2b0a39c62354ad89f4
chipslays <gorshkov34@gmail.com>
2022-04-29 09:11:09 +0400

    add empty rules

--- examples/basic/Events/PingEvent.php
+++ examples/basic/Events/PingEvent.php
@@ -9,6 +9,8 @@ class PingEvent extends AbstractEvent
 {
     public static string $eventId = 'ping';
 
+    protected array $rules = [];
+
     public function handle(TcpConnection $connection, Payload $payload, Server $server): void
     {
         $this->reply('pong');
