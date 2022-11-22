d304b657da889bbb7bb701ffec5f117044a8644e
chipslays <gorshkov34@gmail.com>
2022-11-22 11:10:49 +0400

    it

---
+++ examples/basic-closure/server.php
@@ -0,0 +1,23 @@
+<?php
+
+use Porter\Server;
+use Porter\Events\Event;
+use Workerman\Worker;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$worker = new Worker('websocket://0.0.0.0:3737');
+
+$server = Server::getInstance();
+$server->setWorker($worker);
+
+$server->on('ping', function (Event $event) {
+    $event->reply('pong');
+});
+
+$server->on('hello to', function (Event $event) {
+    $username = $event->payload->data['username'];
+    $event->reply(data: ['message' => "Hello, {$username}!"]);
+});
+
+$server->start();
