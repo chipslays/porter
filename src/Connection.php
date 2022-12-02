5e68adcca3d3c059cfe5d09f974ac7575c7e9c28
chipslays <gorshkov34@gmail.com>
2022-12-02 10:56:33 +0400

    add docblocks

--- src/Connection.php
+++ src/Connection.php
@@ -4,13 +4,37 @@ namespace Porter;
 
 use Porter\Connection\Channels;
 use Porter\Support\Collection;
-use Workerman\Connection\ConnectionInterface;
 use Workerman\Connection\TcpConnection;
 
 /**
  * @property int $id
  * @property Collection $data
  * @property Channels $channels
+ *
+ * @method int|string getStatus(bool $raw_output = true)
+ * @method bool|null send(mixed $send_buffer, bool $raw = false)
+ * @method string getRemoteIp()
+ * @method int getRemotePort()
+ * @method string getRemoteAddress()
+ * @method string getLocalIp()
+ * @method int getLocalPort()
+ * @method string getLocalAddress()
+ * @method int getSendBufferQueueSize()
+ * @method int getRecvBufferQueueSize()()
+ * @method bool isIpV4()
+ * @method bool isIpV6()
+ * @method void pauseRecv()
+ * @method void baseRead(resource $socket, bool $check_eof = true)
+ * @method void|bool baseWrite()
+ * @method bool doSslHandshake(resource $socket)
+ * @method void pipe(self $dest)
+ * @method void consumeRecvBuffer(int $length)
+ * @method void close(mixed $data = null, bool $raw = false)
+ * @method resource getSocket()
+ * @method void checkBufferWillFull()
+ * @method bool bufferIsFull()
+ * @method bool bufferIsEmpty()
+ * @method void destroy()
  */
 class Connection
 {
