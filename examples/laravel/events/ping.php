76fadf2117244b13eb23a3f818924662359dc32a
chipslays <hello@chipslays.ru>
2022-12-03 23:34:01 +0400

    rename files

---
+++ examples/laravel/events/ping.php
@@ -0,0 +1,18 @@
+<?php
+
+use Porter\Server;
+use Porter\Payload;
+use Porter\Connection;
+use Porter\Events\AbstractEvent;
+
+return new class extends AbstractEvent
+{
+    public string $type = 'ping';
+
+    protected array $rules = [];
+
+    public function handle(Connection $connection, Payload $payload, Server $server)
+    {
+        $this->reply('pong');
+    }
+};
