382f0176aa6baf3941c36862903a960d10f31b72
chipslays <gorshkov34@gmail.com>
2022-12-06 14:38:02 +0400

    update example

--- examples/client-php/client.php
+++ examples/client-php/client.php
@@ -6,16 +6,25 @@ use Workerman\Connection\AsyncTcpConnection;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-// use localhost instead 0.0.0.0 for client
+// use localhost instead 0.0.0.0 for connect in local env
 $client = new Client('ws://localhost:3737');
 
-// send event to server on connect
-$client->onConnected(fn () => $client->event('ping'));
+$client->getWorker()::$logFile = __DIR__ . '/client.log';
 
-// handle answer event from server
-$client->on('pong', function (AsyncTcpConnection $connection, Payload $payload, Client $client) {
-    echo 'PONG!' . PHP_EOL;
+$client->onConnected(function (AsyncTcpConnection $connection) use ($client) {
+    $client->send('hello');
 });
 
-// start listen events from websocket server
+$client->onDisconnected(function (AsyncTcpConnection $connection) use ($client) {
+    $client->send('goodbye');
+});
+
+$client->onError(function (AsyncTcpConnection $connection, $code, $message) use ($client) {
+    $client->send('whoops');
+});
+
+$client->on('ping', function (AsyncTcpConnection $connection, Payload $payload, Client $client) {
+    $client->send('pong');
+});
+
 $client->listen();
