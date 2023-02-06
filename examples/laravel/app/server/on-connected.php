d367e1bebbe78252754b4c9a7acd0cc8eca91e89
chipslays <gorshkov34@gmail.com>
2023-02-06 16:52:28 +0400

    fix: remove unused args

--- examples/laravel/app/server/on-connected.php
+++ examples/laravel/app/server/on-connected.php
@@ -2,6 +2,6 @@
 
 use Porter\Connection;
 
-server()->onConnected(function (Connection $connection, string $header) {
+server()->onConnected(function (Connection $connection) {
     cprint('{text:darkGreen}User connected: ' . $connection->getRemoteAddress());
 });
