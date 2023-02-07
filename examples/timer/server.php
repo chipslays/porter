43bb378cab4c2b5e55239f4f122998f272b5f300
chipslays <gorshkov34@gmail.com>
2023-02-07 16:45:41 +0400

    init

---
+++ examples/timer/server.php
@@ -0,0 +1,17 @@
+<?php
+
+use Workerman\Worker;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$worker = new Worker('websocket://0.0.0.0:3737');
+
+server()->boot($worker)->setLogFile(__DIR__ . '/server.log');
+
+server()->onStart(function () {
+    timer(1, function () {
+        connections()->broadcast('update date', ['date' => date('d.m.Y H:i:s')]);
+    });
+});
+
+server()->start();
