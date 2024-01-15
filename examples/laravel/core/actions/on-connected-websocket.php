38abdab2d9e0f50ba95dd21aef761398afc5d028
chipslays <hello@chipslays.ru>
2024-01-15 16:23:08 +0400

    init 2.x

--- examples/laravel/core/actions/on-connected-websocket.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Porter\Connection;
-
-use function porter\server;
-
-server()->onWebsocketConnected(function (Connection $connection, string $header) {
-    //
-});
--- examples/simple-chat/README.md
+++
@@ -1,4 +0,0 @@
-## Launch
-
-1. Type in Terminal: `php server.php start`;
-2. Open `chat.html` in browser;
